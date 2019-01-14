91aa4aa0d9cb99753002035c9d7caed054358b29
yoyojackson <jason7830@gmail.com>
2019-01-14 14:04:05 +0800

    .

--- DataProcess.py
+++ DataProcess.py
@@ -2,5 +2,19 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("tsec/data/2030.csv")
-df.tail()
+class DataProcessor(object):
+	def __init__(self,path = "tsec/data/2030.csv"):
+		self.path = path
+
+	def show(self):
+		df = pd.read_csv(self.path)
+		print(df.tail())
+
+
+def main():
+	p = DataProcessor()
+	p.show()
+
+
+
+main()
