67999c7084b168fbd535780b37f66bd75453bc1b
yoyojackson <jason7830@gmail.com>
2019-02-27 22:25:23 +0800

    _

--- tsec/LSTM_Stock.py
+++ tsec/LSTM_Stock.py
@@ -1,5 +1,5 @@
 from sklearn.preprocessing import MinMaxScaler
-
+from sklearn.metrics import r2_score
 from keras.models import Sequential
 from keras.layers import LSTM, Activation, Dense
 from keras.optimizers import Adam
@@ -8,11 +8,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#rate: data size for trainning 
 def splitData(X,Y,rate):
-  X_train = X[int(X.shape[0]*rate):]
-  Y_train = Y[int(Y.shape[0]*rate):]
-  X_val = X[:int(X.shape[0]*rate)]
-  Y_val = Y[:int(Y.shape[0]*rate)]
+  X_train = X[:int(X.shape[0]*rate)]
+  Y_train = Y[:int(Y.shape[0]*rate)]
+  X_val = X[int(X.shape[0]*rate):]
+  Y_val = Y[int(Y.shape[0]*rate)-1:]
   return X_train, Y_train, X_val, Y_val
 
 
@@ -21,7 +22,7 @@ def splitData(X,Y,rate):
 df = pd.read_csv("data/2330_indicators.csv")
 X_train = df[:-1].values
 y_train = df.loc[1:,['closed']].values
-X_train , Y_train , X_val , Y_val = splitData(X_train,y_train,0.2)
+X_train , Y_train , X_val , Y_val = splitData(X_train,y_train,0.8)
 
 
 sc = MinMaxScaler()
@@ -42,7 +43,7 @@ for i in range(20,len(X_val_sc)):
 	Y_val_steps.append(Y_val[i])
 X_val , Y_val = np.array(X_val_steps) , np.array(Y_val_steps)
 
-
+df.loc[len(Y_train):].plot()
 print(X_train.shape)
 
 #X_train = np.reshape(X_train,(X_train.shape[0], X_train.shape[1], 1))
@@ -53,12 +54,15 @@ model_lstm.add(LSTM(9,input_shape=X_train.shape[1:],activation='relu',kernel_ini
 model_lstm.add(Dense(1))
 model_lstm.summary()
 model_lstm.compile(loss='mean_squared_error',optimizer='Adam')
-history_model_lstm = model_lstm.fit(X_train, Y_train, epochs=200, batch_size=1, shuffle=False)
+early_stop = EarlyStopping(monitor='loss', patience=5, verbose=1)
+history_model_lstm = model_lstm.fit(X_train, Y_train, epochs=200, batch_size=500, shuffle=False)
 
 y_pred_test_lstm = model_lstm.predict(X_val)
+print(y_pred_test_lstm)
 y_train_pred_lstm = model_lstm.predict(X_train)
 print("The R2 score on the Train set is:\t{:0.3f}".format(r2_score(Y_train, y_train_pred_lstm)))
 r2_train = r2_score(Y_train, y_train_pred_lstm)
+
 """
 print("The Adjusted R2 score on the Train set is:\t{:0.3f}\n".format(adj_r2_score(r2_train, X_train.shape[0], X_train.shape[1])))
 print("The R2 score on the Test set is:\t{:0.3f}".format(r2_score(y_test, y_pred_test_lstm)))
